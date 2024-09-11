5fdcb48b1d58248b2266f906581786fe937dd558
Nathan Corral <nathanbcorral@gmail.com>
2024-09-11 11:18:58 +0200

    added option for continue training and saving ckpt every epoch

--- 02.1_train.py
+++ 02.1_train.py
@@ -42,9 +42,73 @@ def initialize_model(config, num_labels):
     
     return model
 
-def main(config):
+def print_learning_rate(optimizer):
+    for i, param_group in enumerate(optimizer.param_groups):
+        print(f"Learning rate for param group {i}: {param_group['lr']}")
+
+def save_train_ckpt(ckpt_dir, epoch, steps,
+                model, optimizer, scheduler, 
+                train_losses, train_loss_items, 
+                val_losses, val_loss_items):
+    checkpoint = {
+        'epoch': epoch,
+        'steps': steps,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+        # 'scaler_state_dict': scaler.state_dict(), # if using mixed precision
+        'train_losses': train_losses,
+        'train_loss_items': train_loss_items,
+        'val_losses': val_losses,
+        'val_loss_items': val_loss_items,
+    }
+    filename = f'train_ckpt_{epoch:03d}.pth'
+    save_path = os.path.join(ckpt_dir, filename)
+    torch.save(checkpoint, save_path)
+
+def get_most_recent_checkpoint(ckpt_dir):
+    checkpoint_files = [f for f in os.listdir(ckpt_dir) if f.endswith('.pth') and "train" in f]
+    
+    if not checkpoint_files:
+        return None
+    
+    # Sort files by epoch number, assuming filenames are in the form 'train_ckpt_###.pth'
+    checkpoint_files.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
+    
+    most_recent_checkpoint = checkpoint_files[-1]  # The latest checkpoint
+    return os.path.join(ckpt_dir, most_recent_checkpoint)
+
+def restore(ckpt_dir, model, optimizer, scheduler, device):
+    ckpt_path = get_most_recent_checkpoint(ckpt_dir)
+    if not ckpt_path:
+        raise RuntimeError(f'Tried to continue training but no ckpt found in {ckpt_dir}')
+    checkpoint = torch.load(ckpt_path, map_location="cpu")
+    print(f'Continueing training from:  {ckpt_path}')
+
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    # scaler.load_state_dict(checkpoint['scaler_state_dict'], map_location=device)
+
+    # Step the scheduler to catch up learing rate.  If not loading state dict
+    # for _ in range(start_epoch):
+    #     scheduler.step()
+
+    # start_epoch = checkpoint['epoch']
+    # cur_steps = checkpoint['steps']
+
+    # train_losses = checkpoint['train_losses']
+    # train_loss_items = checkpoint['train_loss_items']
+    # val_losses = checkpoint['val_losses']
+    # val_loss_items = checkpoint['val_loss_items']
+    return checkpoint
+
+
+
+def main(config, continue_training=False):
     set_random_seed(5)
     save_dir = config["save_model_dir"]
+    device = config["device"]
 
     datasets = load_dataset(config['ds_name'], name=config["ds_name_arg"], cache_dir=config["dataset_cache_dir"])
 
@@ -56,6 +120,7 @@ def main(config):
 
     train_dataloader = prepare_dataloader(train_dataset, processor, batch_size=config['train_batch_size'], shuffle=True)
     # val_dataloader = prepare_dataloader(val_dataset, processor, batch_size=config['val_batch_size'], shuffle=False)
+    val_dataloader = None # No validation dataset in this database 
 
     model = initialize_model(config, num_labels=len(id2label))
     total, trainable, frac = param_count(model)
@@ -72,10 +137,23 @@ def main(config):
     ckpt_dir = os.path.join(save_dir, f'ckpts/')
     make_folder(ckpt_dir)
 
-    train_losses, val_losses = [], []
-    train_loss_items, val_loss_items = {}, {}
-    for epoch in range(1, config['epochs'] + 1):
+    # Reload if we are continueing training
+    if continue_training:
+        checkpoint = restore(ckpt_dir, model, optimizer, scheduler, device)
+        start_epoch = checkpoint['epoch']
+        cur_steps = checkpoint['steps']
+        train_losses = checkpoint['train_losses']
+        train_loss_items = checkpoint['train_loss_items']
+        val_losses = checkpoint['val_losses']
+        val_loss_items = checkpoint['val_loss_items']
+    else:
+        start_epoch, cur_steps = 0, 0
+        train_losses, val_losses = [], []
+        train_loss_items, val_loss_items = {}, {}
+
+    for epoch in range(start_epoch+1, config['epochs']+1):
         print(f'Epoch: {epoch}')
+        print_learning_rate(optimizer)
         train_loss, train_loss_dict = train_one_epoch(model, train_dataloader, optimizer, scheduler, config['device'])
         # Print Epoch Loss
         print(f'Training Loss: {train_loss:.3f}')
@@ -89,33 +167,42 @@ def main(config):
             else:
                 train_loss_items[key].append(value)
         
-        # val_loss, val_loss_dict = validate_one_epoch(model, val_dataloader, config['device'])
-        # print(f'Validation Loss: {val_loss:.3f}')
-        # for k,v in val_loss_dict.items(): 
-        #     print(f'\tval_{k}:   {v:.3f}')
-        # val_losses.append(val_loss)
-        # for key, value in val_loss_dict.items():
-        #     if key not in val_loss_items.keys():
-        #         val_loss_items[key] = [value]
-        #     else:
-        #         val_loss_items[key].append(value)
+        if val_dataloader:
+            val_loss, val_loss_dict = validate_one_epoch(model, val_dataloader, config['device'])
+            print(f'Validation Loss: {val_loss:.3f}')
+            for k,v in val_loss_dict.items(): 
+                print(f'\tval_{k}:   {v:.3f}')
+            val_losses.append(val_loss)
+            for key, value in val_loss_dict.items():
+                if key not in val_loss_items.keys():
+                    val_loss_items[key] = [value]
+                else:
+                    val_loss_items[key].append(value)
 
-    # Save the model and training params
+        cur_steps += len(train_dataloader)
+        save_train_ckpt(ckpt_dir, epoch, cur_steps,
+            model, optimizer, scheduler,
+            train_losses, train_loss_items,
+            val_losses, val_loss_items)
+
+    # Save just the model
     model.save_pretrained(ckpt_dir, from_pt=True)
 
-    # Save the training loss/outputs
+    # Save the final training loss/outputs
     train_loss_items["loss"] = train_losses
     val_loss_items["loss"] = val_losses
     save_losses(train_loss_items, config["train_losses"])
-    # save_losses(val_loss_items, config["val_losses"])
+    if val_dataloader:
+        save_losses(val_loss_items, config["val_losses"])
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fine tune the DETR model")
     parser.add_argument('dir', type=str, help="Target folder to save the configuration, models, and outputs")
+    parser.add_argument('--continue', dest="continue_training", action="store_true", help="Continue training from the dir")
     args = parser.parse_args()
 
     if not args.dir.endswith("/"):
         args.dir += "/"
 
     config = load_config(args.dir)
-    main(config)
+    main(config, continue_training=args.continue_training)
